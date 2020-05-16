15f79bffce2efaa486b4749aabcf8f1d7051776a
Bojan Adzievski <bojana@tabtale.com>
2020-05-16 08:21:52 +0200

    two-sum go map solution

--- go/two-sum/README.md
+++ go/two-sum/README.md
@@ -1,4 +1,4 @@
-# Problem
+# [Problem](https://leetcode.com/problems/two-sum)
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
--- go/two-sum/two-sum.go
+++ go/two-sum/two-sum.go
@@ -1,23 +1,13 @@
-package main
-
-import (
-	"fmt"
-)
+package twosum
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if (nums[i] + nums[j]) == target {
-				return []int{i, j}
-			}
+	numsToIndexes := map[int]int{}
+	for i, num := range nums {
+		remainder := target - num
+		if j, ok := numsToIndexes[remainder]; ok {
+			return []int{j, i}
 		}
+		numsToIndexes[num] = i
 	}
 	return []int{}
-}
-
-func main() {
-	nums := []int{2, 7, 11, 15, 8, 12, 13, 90, 22, 32, 44}
-	target := 19
-	result := twoSum(nums, target)
-	fmt.Println(target, nums, result)
 }
