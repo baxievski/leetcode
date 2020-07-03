2765caa4fd36e8fee9e872fc2d0c4f1aa23f321c
Bojan Adzievski <bojana@tabtale.com>
2020-07-03 21:34:43 +0200

    reformat

--- go/011 - container-with-most-water/container-with-most-water.go
+++ go/011 - container-with-most-water/container-with-most-water.go
@@ -2,20 +2,22 @@ package containerwithmostwater
 
 func maxArea(height []int) int {
 	left, right := 0, len(height)-1
-	l, r := left, right
-	ar := area(height, l, r)
-	for l < r-1 {
+	ar := area(height, left, right)
+
+	for l, r := left, right; l < r-1; {
 		if height[l] < height[r] {
 			l++
 		} else {
 			r--
 		}
+
 		cAr := area(height, l, r)
 		if cAr > ar {
 			ar = cAr
 			left, right = l, r
 		}
 	}
+
 	return ar
 }
 
